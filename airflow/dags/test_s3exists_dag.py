69cdf95e6d19156b9de5a5888528a40d596d6ea7
Kim Littlejohn <jkldatascience@gmail.com>
2019-08-14 07:59:40 +0100

    initial commit

---
+++ airflow/dags/test_s3exists_dag.py
@@ -0,0 +1,100 @@
+from __future__ import print_function
+from builtins import range
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator,
+                                S3DataExistsOperator)
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.models import DAG
+from datetime import datetime, timedelta
+
+
+import os
+import time
+from pprint import pprint
+
+seven_days_ago = datetime.combine(
+        datetime.today() - timedelta(7), datetime.min.time())
+schedule_interval = '0 0 1 * *'  # Schedule to run at midnight on the 1st of each month
+s3_sas_data = 'capstone_etl/data/sas_data/{execution_date.year}/{execution_date.month:02d}'
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 0,
+    'catchup': False
+}
+
+
+dag = DAG(
+    dag_id='test_s3exists_dag20',
+    start_date=datetime(2016,2,1), # Start on first of Feb to process Jan data
+    end_date=datetime(2016,4,1), # Currently only have data until Dec 2016
+    catchup=True,
+    default_args=default_args,
+    description='Test out the s3 checks',
+    schedule_interval='@monthly')
+
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+check_lookupi94port_s3  = S3DataExistsOperator(
+    task_id='check_lookupi94port_on_s3',
+    dag=dag,
+    aws_conn_id="aws_conn_id",
+    bucket='dysartcoal-dend-uswest2',
+    prefix='capstone_etl/data/lookup_data',
+    key = 'i94port_codes.csv'
+)
+
+check_lookupi94citres_s3  = S3DataExistsOperator(
+    task_id='check_lookupi94citres_on_s3',
+    dag=dag,
+    aws_conn_id="aws_conn_id",
+    bucket='dysartcoal-dend-uswest2',
+    prefix='capstone_etl/data/lookup_data',
+    key = 'i94cit_i94res_codes.csv'
+)
+
+check_analyticsflightno_s3  = S3DataExistsOperator(
+    task_id='check_analyticsflightno_on_s3',
+    dag=dag,
+    aws_conn_id="aws_conn_id",
+    bucket='dysartcoal-dend-uswest2',
+    prefix='capstone_etl/data/analytics_data/flight',
+    key = 'flightno.csv'
+)
+
+check_sascsv_s3  = S3DataExistsOperator(
+    task_id='check_sascsv_on_s3',
+    dag=dag,
+    aws_conn_id="aws_conn_id",
+    bucket='dysartcoal-dend-uswest2',
+    prefix=s3_sas_data,
+    wildcard_key = '*.csv'
+)
+
+
+def print_context(ds, **kwargs):
+    pprint(kwargs)
+    print('The year is {}'.format(ds[:4]))
+    print('The month is {}'.format(ds[5:7]))
+    print('The time stamp for this execution is: {}'.format(ds))
+    for key, value in os.environ.items():
+        print(f'{key}:{value}')
+
+    return 'Whatever you return gets printed in the logs'
+
+run_this = PythonOperator(
+    task_id='print_the_context',
+    provide_context=True,
+    python_callable=print_context,
+    dag=dag)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> check_lookupi94port_s3 >> end_operator
+start_operator >> check_lookupi94citres_s3 >> end_operator
+start_operator >> check_analyticsflightno_s3 >> end_operator
+start_operator >> check_sascsv_s3  >> end_operator
+start_operator >> run_this >> end_operator
---
+++ airflow/plugins/operators/s3data_exists.py
@@ -0,0 +1,68 @@
+from airflow.hooks.S3_hook import S3Hook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
+import os
+
+class S3DataExistsOperator(BaseOperator):
+
+    ui_color = '#A6E6A6'
+    template_fields = ("prefix",)
+
+    @apply_defaults
+    def __init__(self,
+                aws_conn_id="",
+                bucket=None,
+                prefix=None,
+                key=None,
+                wildcard_key = None,
+                 *args, **kwargs):
+
+        super(S3DataExistsOperator, self).__init__(*args, **kwargs)
+        self.aws_conn_id = aws_conn_id
+        self.bucket=bucket
+        self.prefix=prefix
+        self.key=key
+        self.wildcard_key=wildcard_key
+
+    def execute(self, context):
+        self.log.info(f'S3DataExistsOperator')
+        s3_hook = S3Hook(aws_conn_id=self.aws_conn_id)
+        rendered_prefix = self.prefix.format(**context)
+
+        success = s3_hook.check_for_bucket(self.bucket)
+        if success:
+            self.log.info("Found the bucket: {}".format(self.bucket))
+        else:
+            self.log.info("Invalid bucket: {}".format(self.bucket))
+            raise FileNotFoundError("No S3 bucket named {}".format(self.bucket))
+
+        success = s3_hook.check_for_prefix(prefix=rendered_prefix,
+                                        delimiter='/',
+                                        bucket_name=self.bucket)
+        if success:
+            self.log.info("Found the prefix: {}".format(rendered_prefix))
+        else:
+            self.log.info("Invalid prefix: {}".format(rendered_prefix))
+            raise FileNotFoundError("No prefix named {}/{} ".format(self.bucket, rendered_prefix))
+
+        if self.wildcard_key is not None:
+            full_key = os.path.join(rendered_prefix, self.wildcard_key)
+            success = s3_hook.check_for_wildcard_key(wildcard_key = full_key,
+                                            bucket_name=self.bucket,
+                                            delimiter='/')
+            if success:
+                self.log.info("Found the key: {}".format(full_key))
+            else:
+                self.log.info("Invalid key: {}".format(full_key))
+                raise FileNotFoundError("No key named {}/{} ".format(self.bucket, full_key))
+
+        if self.key is not None:
+            full_key = os.path.join(rendered_prefix, self.key)
+            success = s3_hook.check_for_key(key = full_key,
+                                            bucket_name=self.bucket)
+            if success:
+                self.log.info("Found the key: {}".format(full_key))
+            else:
+                self.log.info("Invalid key: {}".format(full_key))
+                raise FileNotFoundError("No key named {}/{} ".format(self.bucket, full_key))
